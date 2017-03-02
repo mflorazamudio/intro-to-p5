be0418027435633c439fd885ba7b03f66a4e81c8
Maria Flora Zamudio <mflorazamudio@wlapcs.org>
2017-03-02 02:00:12 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,43 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    line(35,80,300,200);
+    
+    line(45,250,300,200);
+    
+    line(255,155,500,150);
+    
+    line(485,155,485,320);
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(250,130,90,140);
+    
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(86,8,142);
+    ellipse(50,250,80,80);
+    
+
+    
+    stroke(255,255,255);
+    fill(5,113,137);
+    rect(30,50,90,140);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(25,193,140);
+    ellipse(490,150,80,80);
+    
+
+    
+}
+// draw 2 ellipes//
+//draw 2 rectangles//
+//draw 2 lines (1 with a postive slope, 1 with a negative slope, 1 with a slope of 0, and 1 with a slope of undefined.)//
+
+line(155,255,155,390);
